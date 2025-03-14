d5f46a162087833a954f3592af72efc522a1ff03
LEYMER222 <abdullohshaymanov222@gmail.com>
2025-03-14 18:57:26 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let nameK = prompt('Ismingizni kiriting')
+console.log(nameK);
+
+let firnm = prompt('Familiyangizni kiriting')
+console.log(firnm);
+
+let  age = prompt('yoshingizni kiriting')
+console.log(age);
+
+let  fan = prompt('yoqtirgan faningizni kiriting' , 'IT')
+console.log(fan);
+
+let shahar = prompt('Yashaydigan shaharingizni kiriting' ,'Toshkent')
+console.log(shahar);
+
+alert(`Salom mening ismim ${nameK},va men ${age} yosh daman.Men ${shahar} daman!`)
